02cf99a270f29d736f61c8a4c3d9c49c64bee934
demi <idd90i@gmail.com>
2017-05-03 23:28:01 +0300

    Lesson 8

--- common/src/main/java/ru/javaops/masterjava/config/Configs.java
+++ common/src/main/java/ru/javaops/masterjava/config/Configs.java
@@ -3,17 +3,23 @@ package ru.javaops.masterjava.config;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
-/**
- * gkislin
- * 01.11.2016
- */
+import java.io.File;
+
 public class Configs {
 
-    public static Config getConfig(String resource) {
-        return ConfigFactory.parseResources(resource).resolve();
-    }
+	public static Config getConfig(String resource) {
+		return ConfigFactory.parseResources(resource).resolve();
+	}
 
-    public static Config getConfig(String resource, String domain) {
-        return getConfig(resource).getConfig(domain);
-    }
+	public static Config getConfig(String resource, String domain) {
+		return getConfig(resource).getConfig(domain);
+	}
+
+	public static File getConfigFile(String path) {
+		return new File(AppConfig.APP_CONFIG.getString("configDir"), path);
+	}
+
+	private static class AppConfig {
+		private static final Config APP_CONFIG = getConfig("app.conf", "app");
+	}
 }
--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
@@ -1,25 +1,25 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableList;
+import ru.javaops.masterjava.config.Configs;
 import ru.javaops.masterjava.persist.DBITestProvider;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.ws.Endpoint;
-import java.io.File;
 import java.util.List;
 
 public class MailServicePublisher {
 
-    public static void main(String[] args) {
-        DBITestProvider.initDBI();
+	public static void main(String[] args) {
+		DBITestProvider.initDBI();
 
-        Endpoint endpoint = Endpoint.create(new MailServiceImpl());
-        List<Source> metadata = ImmutableList.of(
-                new StreamSource(
-                        new File("services/mail-service/src/main/webapp/WEB-INF/wsdl/mailService.wsdl")));
+		Endpoint endpoint = Endpoint.create(new MailServiceImpl());
+		List<Source> metadata = ImmutableList.of(
+			new StreamSource(Configs.getConfigFile("wsdl/mailService.wsdl")),
+			new StreamSource(Configs.getConfigFile("wsdl/common.xsd")));
 
-        endpoint.setMetadata(metadata);
-        endpoint.publish("http://localhost:8080/mail/mailService");
-    }
+		endpoint.setMetadata(metadata);
+		endpoint.publish("http://localhost:8080/mail/mailService");
+	}
 }
